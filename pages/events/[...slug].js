230cf28f5eea05037ea959a8bbf9bb1d7a66675d
elvisonob <elvisonob@yahoo.com>
2023-05-29 08:04:08 +0100

    adding slug

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,13 @@
+import { useRouter } from 'next/router';
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events</h1>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,23 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+
 const AllEventsPage = () => {
+  const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <Fragment>
+      <EventsSearch onSearch={findEventHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 };
 
