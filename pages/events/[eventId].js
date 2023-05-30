b46dcb2db8da9aa0d321be929daeab323f5e4cf4
elvisonob <elvisonob@yahoo.com>
2023-05-30 06:11:09 +0100

    wrapping up

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
 import { getEventById } from '../../dummy-data';
+import ErrorAlert from '../../components/ui/error-alert';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
@@ -13,7 +14,11 @@ const EventDetailPage = () => {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No event found!</p>;
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
   }
 
   return (
