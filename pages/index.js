3daa5118b315d91ea5033288de9bd7db2a959008
elvisonob <elvisonob@yahoo.com>
2023-06-05 08:37:27 +0100

    adding a dummy backend in database

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/event-list';
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
+const HomePage = (props) => {
+  //const featuredEvents = getFeaturedEvents();
+  const itemsFeatured = props.items1.filter((event) => event.isFeatured);
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={itemsFeatured} />
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch(
+    'https://nextjs-88542-default-rtdb.firebaseio.com/events.json'
+  );
+
+  const data = await response.json();
+  console.log(data);
+
+  const dataContents = [];
+  for (const key in data) {
+    dataContents.push({
+      id: key,
+      date: data[key].date,
+      description: data[key].description,
+      image: data[key].image,
+      isFeatured: data[key].isFeatured,
+      location: data[key].location,
+      title: data[key].title,
+    });
+  }
+
+  return { props: { items1: dataContents } };
+}
 
 export default HomePage;
